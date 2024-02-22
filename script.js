8fb185424f1ba5caa07b6e7a99c612db3854fee4
VNfifty50 <145123015+VNfifty50@users.noreply.github.com>
2024-02-21 23:27:36 -0300

    Initial commit

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 function getComputerChoice () {
     let arr = ["Rock", "Papers", "Scissors"];
     const random = Math.floor(Math.random() * arr.length);
@@ -5,61 +7,92 @@ function getComputerChoice () {
 }
 
 const btn = document.querySelector("#btn");
-const body = document.querySelector("body");
+const rock = document.querySelector("#rock");
+const papers = document.querySelector("#papers");
+const scissors = document.querySelector("#scissors");
 
+// Start the game logic when the header button is clicked
 btn.addEventListener("click", () => {
-    playGame();
+    alert("The game just started!");
+    startGame();
 })
+
+function playerOption() {
+    let playerSelection = "";
+
+    rock.addEventListener("click", () => {
+        playerSelection = "rock";
+    })
+    papers.addEventListener("click", () => {
+        playerSelection = "papers";
+    })
+    scissors.addEventListener("click", () => {
+        playerSelection = "scissors";
+    })
+
+    if (playerSelection != "") {
+        return playerSelection;
+    }
+}
 
 function playerRound(playerSelection, computerSelection) {
     let winner = "";
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
     if ((playerSelection === "rock" && computerSelection === "scissors") || 
         (playerSelection === "paper" && computerSelection === "rock")    ||
         (playerSelection === "scissors" && computerSelection === "paper")) {
-            return playerSelection;
+            winner =  playerSelection;
     } else if (playerSelection === computerSelection) {
-        return "It's a tie!"
-    } 
-    return computerSelection;
+        winner = "It's a tie!"
+    } else {
+        winner = computerSelection;
+    }
+    return winner;
 }
 
-function playGame() {
-    let counter = 5;
-    let userAnswer = "";
-    let userPoints = 0;
-    let computerPoints = 0;
-    while (counter > 0) {
-        userAnswer = prompt("Rock, paper or scissors?").toLowerCase();
-        let computerAnswer = getComputerChoice();
 
-        let winner = playerRound(userAnswer, computerAnswer);
+function startGame() {
+    let playerPoints = 0;
+    let computerPoints = 0;
+    let counter = 0; //breakpoint to end the loop after a certain number
+    let body = document.querySelector("body");
+
+    // LOOP RUNS FOREVER NEEDS A FIX
+
+    // the rounds only stop when one of the players get 5 points
+    while (playerPoints != 5 || computerPoints != 5 || counter > 2) {
+        let winnerElement = document.createElement("div");
+        let playerSelection = "";
+        if (playerSelection != "") {
+            alert("Choose an option!")
+        }
+        playerSelection = playerOption();
+        let computerSelection = getComputerChoice();
         
-        if (userAnswer == winner) {
-            userPoints++;
-            alert(`You win! ${userAnswer} beats ${computerAnswer}`);
-        } else if (computerAnswer == winner) {
-            computerPoints++;
-            alert(`You lose! ${computerAnswer} beats ${userAnswer}`);
-        } else {
-            alert("It's a tie!");
-        }
-        counter--;
-    }
-    
-    if (userPoints > computerPoints) {
-        if (userPoints > 1) {
-            alert(`You are the winner with ${userPoints} points`);
-        } else {
-            alert(`You are the winner with ${userPoints} point`);
-        }
+        let winner = playerRound(playerSelection, computerSelection);
         
-    } else if (computerPoints > userPoints) {
-        if (computerPoints > 1) {
-            alert(`Computer is the winner with ${computerPoints} points`);
-        } else {
-            alert(`Computer is the winner with ${computerPoints} point`);
+        // alter the dom tree and introduce an element calling the winner..
+        // sum the round points to each counter
+        if (playerSelection != "") {
+            if (winner === playerSelection) {
+                winnerElement.textContent = `You win! ${playerSelection} beats ${computerSelection}!`;
+                playerPoints++;
+            } else if (winner === computerSelection) {
+                winnerElement.textContent = `You lose! ${computerSelection} beats ${playerSelection}!`;
+                computerPoints++;
+            } else {
+                winnerElement.textContent = "It's a tie";
+            }
+
+            body.appendChild(winnerElement);
         }
     }
+    counter++;
+    // Alter the dom tree again to announce the player who got most points
+    if (playerPoints === 5) {
+        alert("you are the champion");
+    } else if (computerPoints === 5) {
+        alert("computer always wins")
+    }
+
+
 }
